664a0a99f56216aa3ad3020af55b3e9b247700b1
ZhangYing <zhangyingsixsixsix@foxmail.com>
2026-04-15 17:56:13 +0800

    day2 asyncio/aiohttp

---
+++ FastAPI/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+
+@app.get("/age")
+def get_age(age: int = 0):
+    return {"年龄": age}
